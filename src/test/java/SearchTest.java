4f1a097b067a23519dda156eb3e9232dd753ae46
Danilasob <sobolev27f950@mail.ru>
2022-09-25 21:19:10 +0300

    Initial commit

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTest {
+    @Test
+    void SelenideSearchTest(){
+        //открыть страницу Гугла
+        open("https://google.com");
+        //Ввыести selenide в поисквоую строку
+        $("[name=q]").setValue("Selenide").pressEnter();
+        //sleep(5000);
+        //Проверить что selenide появилось в результатах поиска
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+}
